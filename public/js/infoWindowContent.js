7643976d837592857cef1fa16013719e96113233
Mike Levine <mike.levine@gmail.com>
2018-03-19 01:21:32 -0400

    adding titles

--- public/js/infoWindowContent.js
+++ public/js/infoWindowContent.js
@@ -103,7 +103,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/vaalmarch022210/IMG_1066.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>Residents and APF affiliates held a demonstration in front of the Sedibeng District Municipality Office ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=vaalmarch022210&id=10">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=vaalmarch022210&id=11">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -112,7 +112,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/sharpevillememorial032110/IMG_1206.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>Commemoration of the 50th Anniversary of the Sharpeville Massacre  ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=sharpevillememorial032110&id=11">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=sharpevillememorial032110&id=12">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -121,7 +121,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '' +
                 '<p>People Opposing Women Abuse (POWA) launched its 2009-2010 Breaking The Silence anthology ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=powabooklaunch&id=12">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=powabooklaunch&id=13">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -130,7 +130,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/vaalmarchtoarcelormittal051110/IMG_1130.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>Community affiliates across the Vaal Region held a demonstration at the west entrance ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=vaalmarchtoarcelormittal051110&id=13">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=vaalmarchtoarcelormittal051110&id=14">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -139,7 +139,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '' +
                 '<p>The Curriculum Development Programme (CDP) is an arts-based training organization ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=visittocdp&id=14">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=visittocdp&id=15">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -148,7 +148,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/worldcupmarch061110/IMG_1498.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>APF planned a national program of action to express their grievances ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=worldcupmarch061110&id=15">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=worldcupmarch061110&id=16">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -157,7 +157,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '' +
                 '<p>June 16 marks the anniversary of the Soweto Uprising, when in 1976 police opened fire on students ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=youthday&id=16">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=youthday&id=17">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -166,7 +166,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/joziregionalhousingmarch062510/IMG_1588.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>APF\'s Johannesburg region coordinated a demonstration in front of the Johannesburg Department of Housing ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=joziregionalhousingmarch062510&id=17">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=joziregionalhousingmarch062510&id=18">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -175,7 +175,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/schubartparkanti-xenophobiaevent071110/IMG_2183.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>Schubart Park, a APF affiliate in the Tshwane region held a program to draw attention to concerns ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=schubartparkanti-xenophobiaevent071110&id=18">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=schubartparkanti-xenophobiaevent071110&id=19">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -184,7 +184,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/quaggaevictions080310/IMG_2306.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>Quagga Estates, an apartment complex on the outskirts of Pretoria, had been sold to private owners ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=schubartparkanti-quaggaevictions080310&id=19">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=quaggaevictions080310&id=20">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -193,7 +193,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/sowetomarch091510/IMG_2358.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>In Dube, Soweto, Soweto Electricity Crisis Committee (SECC) and Soweto Concerned Residents (SCR) held a march ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=schubartparkanti-sowetomarch091510&id=20">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=schubartparkanti-sowetomarch091510&id=21">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -202,7 +202,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/silentmarchfxi101910/IMG_2451.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>About 600 people marched from Jorrison Street to Constitutional Hill to protest ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=silentmarchfxi101910&id=21">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=silentmarchfxi101910&id=22">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -211,7 +211,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '<img src="https://s3-us-west-2.amazonaws.com/scrmarch112409/IMG_0717.jpg" alt="Porcelain Factory of Vista Alegre" height="115">' +
                 '<p>As I attended a meeting of the Soweto Concerned Residents, I was shown newspaper coverage of protests ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=scrmarch112409&id=22">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=scrmarch112409&id=23">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>'],
@@ -220,7 +220,7 @@ var infoWindowContent = [
               '<div class="iw-content">' +
                 '' +
                 '<p>At a march and demonstration in front of the police station in Rockville organized by the Soweto Electricity Crisis Committee ...</p>' +
-                '<a class="iw-subTitle btn btn-primary" href="/media?event=seccmarch&id=23">Learn More</a>' +
+                '<a class="iw-subTitle btn btn-primary" href="/media?event=seccmarch&id=24">Learn More</a>' +
               '</div>' +
               '<div class="iw-bottom-gradient"></div>' +
             '</div>']
